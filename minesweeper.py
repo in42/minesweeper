a39cddebb1422cbcd0d79e9b88a99a9bde011b14
Vikram Pal <vikram.invincible.pal4@gmail.com>
2016-10-22 07:37:01 +0530

    Added keeping count of mines in adjacent cells

--- minesweeper.py
+++ minesweeper.py
@@ -4,14 +4,18 @@ class MinesweeperCell:
     def __init__(self):
         self.has_mine = False
         self.is_open = False
+        self.n_adjacent_mines = 0
 
     def get_as_string(self):
         if not self.is_open:
             return '#'
         elif self.has_mine:
             return '*'
+        elif self.n_adjacent_mines is not 0:
+            return str(self.n_adjacent_mines)
         else:
             return '.'
+
 
 class MinesweeperBoard:
     def __init__(self, width, height):
@@ -36,12 +40,24 @@ class MinesweeperBoard:
                 if not self.board[y][x].has_mine:
                     break
             self.board[y][x].has_mine = True
+            self._increase_adjacent_mine_counts(x, y)
 
     def print_all_cells(self):
         for y in range(self.height):
             for x in range(self.width):
                 print(self.board[y][x].get_as_string(), end="")
             print()
+
+    def _is_valid_coord(self, x, y):
+        return 0 <= x < self.width and 0 <= y < self.height
+
+    def _increase_adjacent_mine_counts(self, x, y):
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if self._is_valid_coord(y + i, x + j):
+                    self.board[y + i][x + j].n_adjacent_mines = \
+                    self.board[y + i][x + j].n_adjacent_mines + 1
+
 
 #testing
 if __name__ == "__main__":
