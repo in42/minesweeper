afe8f390022a071940592abfc60585af05c22d9c
Vikram Pal <vikram.invincible.pal4@gmail.com>
2016-10-28 11:14:42 +0530

    Added marking of cell

--- minesweeper.py
+++ minesweeper.py
@@ -5,12 +5,19 @@ class MinesweeperCell:
         self.has_mine = False
         self.is_open = False
         self.n_adj_mines = 0
+        self.is_marked = False
 
     def __str__(self):
-        if not self.is_open:
+        if not self.is_open and self.is_marked:
+            return '>'
+        elif not self.is_open:
             return '#'
+        elif self.has_mine and self.is_marked:
+            return '>'
         elif self.has_mine:
             return '*'
+        elif not self.has_mine and self.is_marked:
+            return '^'
         else:
             return '.'
 
@@ -47,24 +54,31 @@ class MinesweeperBoard:
                     if (self._is_valid_coord(r + dr, c + dc) and
                         not self.board[r + dr][c + dc].is_open):
                         self.click_on_cell(r + dr, c + dc)
+    
+    def mark_cell(self, r, c):
+        self.board[r][c].is_marked = True
 
-    def print_all_cells(self, show_mine_counts=True):
-        print('  ', end='')
+    def remove_mark_cell(self, r, c):
+        self.board[r][c].is_marked = False
+
+    def print_all_cells(self, is_show_mine_counts=True):
+        format_specifier = '{!s:>3}'
+        line = format_specifier.format('')
         for c in range(self.width):
-            print(' ', end='')
-            print(c, end='')
-        print()
+            line = line + format_specifier.format(c)
+        print(line)
         for r in range(self.height):
-            print(' ', end='')
-            print(r, end='')
+            line = format_specifier.format(r)
             for c in range(self.width):
-                print(' ', end='')
-                if (show_mine_counts and self.board[r][c].is_open and
+                value = ''
+                if (is_show_mine_counts and self.board[r][c].is_open and
+                    not self.board[r][c].has_mine and
                     self.board[r][c].n_adj_mines != 0):
-                    print(self.board[r][c].n_adj_mines, end='')
+                    value = self.board[r][c].n_adj_mines
                 else:
-                    print(self.board[r][c], end='')
-            print()
+                    value = self.board[r][c]
+                line = line + format_specifier.format(value)
+            print(line)
 
     def _is_valid_coord(self, r, c):
         return 0 <= c < self.width and 0 <= r < self.height
@@ -79,7 +93,7 @@ class MinesweeperBoard:
 
 #testing
 if __name__ == "__main__":
-    board = MinesweeperBoard(10, 10)
+    board = MinesweeperBoard(8, 8)
     board.place_mines(10)
     quit = False
     while not quit:
@@ -89,8 +103,17 @@ if __name__ == "__main__":
         if tokens[0] == 'q' or tokens[0] == 'quit':
             quit = True
         elif tokens[0] == 'c' or tokens[0] == 'click':
-            board.click_on_cell(int(tokens[1]), int(tokens[2]))
+            if (board.click_on_cell(int(tokens[1]), int(tokens[2])) == False):
+                print("GAME OVER!")
+                board.open_all()
+                board.print_all_cells()
+                quit = True
         elif (tokens[0] == 's' or tokens[0] == 'show'):
             board.print_all_cells()
+        elif (tokens[0] == 'm' or tokens[0] == 'mark'):
+            board.mark_cell(int(tokens[1]), int(tokens[2]))
+        elif (tokens[0] == 'rm' or
+                tokens[0] + ' ' + tokens[1] == 'remove mark'):
+            board.remove_mark_cell(int(tokens[2]), int(tokens[3]))
     # board.open_all()
     # board.print_all_cells()
